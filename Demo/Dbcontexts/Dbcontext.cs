8e1d320774f51d8ad70dc53d843ad676ce2f02ef
admin <admin@DESKTOP-JKD834T>
2025-03-01 23:52:19 +0200

    Add project files.

---
+++ Demo/Dbcontexts/Dbcontext.cs
@@ -0,0 +1,12 @@
+﻿global using Microsoft.EntityFrameworkCore;
+namespace Demo.Dbcontexts
+{
+    internal class Dbcontext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }   
+}
