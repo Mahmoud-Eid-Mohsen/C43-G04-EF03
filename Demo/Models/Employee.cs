f3e5802142abfb2812a4d1b5b18493ff770e0ada
admin <admin@DESKTOP-JKD834T>
2025-03-02 01:32:25 +0200

    Mapping inhertace

---
+++ Demo/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+        public int EmployeeId { get; set; }
+        public string Email { get; set; }= string.Empty;
+        public string Adress { get; set; } = string.Empty;
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,44 @@
-﻿namespace Demo
+﻿using Demo.Dbcontexts;
+using Demo.Models;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            using Dbcontext dbcontext = new Dbcontext();
+            #region Mapping inhertace
+            // 3 way for mapping inheertance 
+            //1.TPH ->(Default) one table for base and his child
+            //2. TPT -> one table for all type 
+            //3. TPCC => one table for All concreate class
+            var parttime = new PartTimeEmployee() 
+            {
+                Name="mahmoud",
+                Adress="Fayoum",
+                Email="me1673",
+                HourRate=500,
+                HourWorked=500,
+            
+            
+            };
+            var fulltime = new FulltimeEmployee()
+            {
+
+                Name = "Ahmed",
+                Adress = "Fayoum",
+                Email = "Eidm11",
+                MonthlySalary=25000
+
+            };
+            dbcontext.Employees.AddRange(parttime, fulltime);
+            dbcontext.SaveChanges();
+
+
+            #endregion
+
+
         }
     }
 }
